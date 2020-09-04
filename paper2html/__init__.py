a435e86ad74476e03ce01f83035a1b0af7c0673b
kaneko <kaneko@weblab.t.u-tokyo.ac.jp>
2020-09-04 23:32:06 +0900

    change versoin

--- paper2html/__init__.py
+++ paper2html/__init__.py
@@ -1,6 +1,6 @@
 from .commands import open_paper_htmls, message_for_automator, paper2html
 
 name = "paper2html"
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 __all__ = ['open_paper_htmls', 'message_for_automator', 'paper2html']
 
