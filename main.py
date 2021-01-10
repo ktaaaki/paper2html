cee1208b36ff65e0ab958eb3e1ea77e65f53363b
ktaaaki <kaneko@weblab.t.u-tokyo.ac.jp>
2021-01-11 07:53:40 +0900

    config for google app engine (not working)

--- main.py
+++ main.py
@@ -20,13 +20,15 @@ def render():
     _, ext = os.path.splitext(download_url)
     if ext != ".pdf":
         return f"{download_url} is not url to pdf."
-    cache_dir = "paper_cache"
+    cache_dir = "/tmp"
     if not os.path.exists(cache_dir):
+        print('tmp dir does not exists!')
         os.mkdir(cache_dir)
     _, filename = os.path.split(download_url)
     working_dir = os.path.join(cache_dir, urllib.parse.quote(download_url, safe=''))
     pdf_filename = os.path.join(working_dir, filename)
     if not os.path.exists(working_dir):
+        print('creating working dir.')
         # generate converted html on server cache
         os.mkdir(working_dir)
         n_div_paragraph = math.inf
@@ -35,19 +37,23 @@ def render():
         with urllib.request.urlopen(download_url) as uf:
             with open(pdf_filename, 'bw') as of:
                 of.write(uf.read())
+            print('download pdf.')
+            if not os.path.exists(pdf_filename):
+                print('download failed.')
         Paper.n_div_paragraph = n_div_paragraph
         for url in paper2html(pdf_filename, working_dir, line_margin_rate, verbose):
             # return send_file(url)
+            print('output html file')
             pass
     result_dirname, _ = os.path.splitext(filename)
     result_html = os.path.join(working_dir, result_dirname, f"{result_dirname}_0.html")
     # delete cache
-    buffered = BytesIO()
-    with open(result_html, 'rb') as f:
-        buffered.write(f.read())
-    buffered.seek(0)
-    shutil.rmtree(cache_dir)
-    return send_file(buffered, mimetype='text/html')
+    with BytesIO() as buffered:
+        with open(result_html, 'rb') as f:
+            buffered.write(f.read())
+        buffered.seek(0)
+        shutil.rmtree(cache_dir)
+        return send_file(buffered, mimetype='text/html')
 
 
 @app.errorhandler(500)
@@ -60,4 +66,5 @@ def server_error(e):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080, host='127.0.0.1')
+    # app.run(debug=True, port=8080, host='127.0.0.1')
+    app.run(debug=False, port=80, host='0.0.0.0')
