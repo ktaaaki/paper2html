4afd67924a3f4d9fb38952565a256bf960c2df14
ktaaaki <kaneko@weblab.t.u-tokyo.ac.jp>
2021-03-10 01:53:46 +0900

    bug fix of LocalPaperDirectory

--- paper2html/convert_service.py
+++ paper2html/convert_service.py
@@ -12,23 +12,6 @@ from paper2html import paper2html
 from paper2html.paper import Paper
 
 
-def init_cache_dir():
-    cache_dir = "paper_cache"
-    if not os.path.exists(cache_dir):
-        print('tmp dir does not exists!')
-        os.mkdir(cache_dir)
-    return cache_dir
-
-
-def init_working_dir(cache_dir, filename):
-    basename, _ = os.path.splitext(filename)
-    working_dir = os.path.join(cache_dir, basename)
-    if not os.path.exists(working_dir):
-        print('creating working dir.')
-        os.mkdir(working_dir)
-    return working_dir
-
-
 def paper2one_html(src_path, cache_dir, debug):
     line_margin_rate = None
     verbose = debug
@@ -50,51 +33,78 @@ def download(url, dir_path):
     return file_path
 
 
+class TemporaryDownloader:
+    def __init__(self, url, temp_dir):
+        self.url = url
+        self.temp_dir = temp_dir
+        self.downloaded_path = None
+
+    def __enter__(self):
+        if not os.path.exists(self.temp_dir):
+            os.mkdir(self.temp_dir)
+        self.downloaded_path = download(self.url, self.temp_dir)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        shutil.rmtree(self.temp_dir)
+
+
 class LocalPaperDirectory:
-    # ディレクトリの状況に同期，管理を行う予定，
-    def __init__(self, watch):
-        cache_dir = init_cache_dir()
+    def __init__(self, watch, debug=False):
+        self._init_cache_dir()
         self.obs = None
+        self.debug = debug
         if watch:
-            self.start_watching(cache_dir)
+            self.start_watching()
 
     def __del__(self):
-        if self.obs:
-            self.stop_watching()
+        self.stop_watching()
+
+    def _init_cache_dir(self):
+        cache_dir = "paper_cache"
+        if not os.path.exists(cache_dir):
+            print('tmp dir does not exists!')
+            os.mkdir(cache_dir)
+        self.cache_dir = cache_dir
+
+    def _get_working_dir(self, filename):
+        basename, _ = os.path.splitext(filename)
+        working_dir = os.path.join(self.cache_dir, basename)
+        return working_dir
+
+    def _init_working_dir(self, filename):
+        working_dir = self._get_working_dir(filename)
+        if not os.path.exists(working_dir):
+            print('creating working dir.')
+            os.mkdir(working_dir)
+        return working_dir
+
+    def _download2working_dir(self, url):
+        _, filename = os.path.split(url)
+        working_dir = self._init_working_dir(filename)
+        return download(url, working_dir)
+
+    def _store_pdf(self, pdf_path):
+        _, filename = os.path.split(pdf_path)
+        stored_pdf_path = os.path.join(self._init_working_dir(filename), filename)
+        shutil.move(pdf_path, stored_pdf_path)
 
     def get_result_html_path(self, url_or_path):
-        cache_dir = init_cache_dir()
         _, filename = os.path.split(url_or_path)
-        working_dir = init_working_dir(cache_dir, filename)
+        working_dir = self._get_working_dir(filename)
         basename, _ = os.path.splitext(filename)
         # cache_dir/basename/basename_0.html
         result_html = os.path.join(working_dir, f"{basename}_0.html")
         return result_html
 
-    def download2working_dir(self, url):
-        cache_dir = init_cache_dir()
-        _, filename = os.path.split(url)
-        working_dir = init_working_dir(cache_dir, filename)
-        return download(url, working_dir)
-
     def is_converted(self, url_or_path):
         return os.path.exists(self.get_result_html_path(url_or_path))
-
-    def prepare_html(self, download_url):
-        if self.is_converted(download_url):
-            # cache_dir/basename/basename_0.html
-            return self.get_result_html_path(download_url)
-        else:
-            pdf_filename = self.download2working_dir(download_url)
-            result_html = paper2one_html(pdf_filename, cache_dir, debug)
-            print('output html file')
-            return result_html
 
     def _is_time_to_convert(self, event):
         base_dir, filename = os.path.split(event.src_path)
 
         # to ignore intermediate pdf files on some OS (recursive option does not work... ?)
-        if not os.path.samefile(base_dir, init_cache_dir()):
+        if not os.path.samefile(base_dir, self.cache_dir):
             return False
 
         # already converted
@@ -103,29 +113,40 @@ class LocalPaperDirectory:
 
         return True
 
-    def _store_pdf(self, pdf_path):
-        _, filename = os.path.split(pdf_path)
-        stored_pdf_path = os.path.join(init_working_dir(init_cache_dir(), filename), filename)
-        shutil.move(pdf_path, stored_pdf_path)
+    def prepare_html(self, download_url):
+        if self.is_converted(download_url):
+            # cache_dir/basename/basename_0.html
+            return self.get_result_html_path(download_url)
+        else:
+            _, filename = os.path.split(download_url)
+            temp_dir = os.path.join(self.cache_dir, filename)
+            # pdf_filename = self._download2working_dir(download_url)
+            with TemporaryDownloader(download_url, temp_dir) as dl:
+                result_html = paper2one_html(dl.downloaded_path, self.cache_dir, self.debug)
+                self._store_pdf(dl.downloaded_path)
+                print('output html file')
+                # TODO: result_html is wrong
+                return self.get_result_html_path(download_url)
 
     def on_pdf_placed(self, event: FileSystemEvent):
         if self._is_time_to_convert(event):
-            paper2one_html(event.src_path, init_cache_dir(), self.debug)
+            paper2one_html(event.src_path, self.cache_dir, self.debug)
             self._store_pdf(event.src_path)
 
-    def start_watching(self, dir_path):
+    def start_watching(self):
         obs = Observer()
-        event_handler = PdfFilePlacedEventHandler(self.on_pdf_placed,
-            server_mode=(str(type(obs)) == "<class 'watchdog.observers.inotify.InotifyObserver'>"))
-        obs.schedule(event_handler, os.path.abspath(dir_path), recursive=False)
+        event_handler = PdfFilePlacedEventHandler(
+            self.on_pdf_placed, server_mode=(str(type(obs)) == "<class 'watchdog.observers.inotify.InotifyObserver'>"))
+        obs.schedule(event_handler, os.path.abspath(self.cache_dir), recursive=False)
         obs.start()
         return obs
 
     def stop_watching(self):
-        obs = self.obs
-        obs.unschedule_all()
-        obs.stop()
-        obs.join()
+        if self.obs:
+            obs = self.obs
+            obs.unschedule_all()
+            obs.stop()
+            obs.join()
 
 
 class PdfFilePlacedEventHandler(PatternMatchingEventHandler):
@@ -151,9 +172,10 @@ class PdfFilePlacedEventHandler(PatternMatchingEventHandler):
 
 
 def convert_service_run(host, port, watch, debug):
-    paper_dir = LocalPaperDirectory(watch)
+    paper_dir = LocalPaperDirectory(watch, debug)
 
     app = Flask(__name__)
+
     @app.route('/paper2html')
     def render():
         download_url = request.args.get('url')
@@ -163,7 +185,7 @@ def convert_service_run(host, port, watch, debug):
             return f"{download_url} is not url to pdf."
 
         result_html = paper_dir.prepare_html(download_url)
-        return send_file(result_html, mimetype='text/html')
+        return send_file(os.path.abspath(result_html), mimetype='text/html')
 
     @app.errorhandler(500)
     def server_error(e):
