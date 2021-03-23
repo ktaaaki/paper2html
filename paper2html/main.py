6f2569dd419d243eecc0b614fe95897329108ddb
ktaaaki <kaneko@weblab.t.u-tokyo.ac.jp>
2021-03-24 01:03:00 +0900

    directory option

--- paper2html/main.py
+++ paper2html/main.py
@@ -3,11 +3,12 @@ from paper2html.convert_service import convert_service_run
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--debug", type=bool, default=False)
     parser.add_argument("--host", type=str, default="127.0.0.1", help="use 0.0.0.0 if you use it on a server.")
     parser.add_argument("--port", type=int, default=5000, help="use 80 if you use it on a server.")
+    parser.add_argument("--dir", type=str, default=None, help="paper storage.")
     parser.add_argument("--watch", type=bool, default=False,
                         help="automatically convert local PDFs on the paper_cache directory.")
+    parser.add_argument("--debug", type=bool, default=False)
     args = parser.parse_args()
 
     convert_service_run(args.host, args.port, args.watch, args.debug)
