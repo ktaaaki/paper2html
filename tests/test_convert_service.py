ae21a3dacfbef20efed43986d1cbf312b8ff3cda
ktaaaki <kaneko@weblab.t.u-tokyo.ac.jp>
2021-03-24 08:40:37 +0900

    test convert_service_run

--- tests/test_convert_service.py
+++ tests/test_convert_service.py
@@ -1,19 +1,29 @@
-import os
 import time
-import subprocess
+import os
+from os.path import join as pjoin, abspath
+import pathlib
+from multiprocessing import Process
+import selenium.common.exceptions
 from selenium import webdriver
 import chromedriver_binary  # Adds chromedriver binary to path
 
+import paper2html
+
 
 def test_convert_service_run():
-    p = subprocess.Popen(['python', os.path.join('paper2html', 'main.py')], shell=False)
+    p = Process(target=paper2html.convert_service_run, args=("0.0.0.0", 5000, None, True, False))
+    p.start()
     driver = webdriver.Chrome()
     driver.get('http://localhost:5000/paper2html/index.html')
     time.sleep(1)
-    driver.get("http://localhost:5000/paper2html/convert?url=https%3A%2F%2Farxiv.org%2Fpdf%2F1609.05473.pdf")
-    # search_box = driver.find_element_by_name("q")
-    # search_box.send_keys('ChromeDriver')
-    # search_box.submit()
-    # time.sleep(1)
-    driver.quit()
-    p.kill()
+    test_dir, _ = os.path.split(__file__)
+    pdf_uri = pathlib.Path(abspath(pjoin(test_dir, 'sample_files', 'one_column.pdf'))).as_uri()
+    url = f"http://localhost:5000/paper2html/convert?url={pdf_uri}"
+    driver.get(url)
+    try:
+        driver.find_element_by_id("right")
+    except selenium.common.exceptions.NoSuchElementException:
+        raise Exception("no output html")
+    finally:
+        driver.close()
+        p.terminate()
